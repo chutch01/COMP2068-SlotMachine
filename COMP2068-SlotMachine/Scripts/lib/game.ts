7b1ddb5b48464701064af700b19cbc72e45b26f8
chutch01 <chutch01@hotmail.com>
2015-02-12 14:38:14 -0500

    initial commit

---
+++ COMP2068-SlotMachine/Scripts/lib/game.ts
@@ -0,0 +1,20 @@
+﻿var canvas;
+var playerMoney = 1000;
+var stage: createjs.Stage;
+var spinButton;
+var creditButton;
+var resetButton;
+var cherry;
+var chest;
+var pikachu;
+var moonstone;
+var replay;
+var seven;
+var fruits;
+
+
+// Game Objects
+var game: createjs.Container;
+var background: createjs.Bitmap;
+var spinBUtton: createjs.Bitmap;
+var tiles: createjs.Bitmap[] = [];
